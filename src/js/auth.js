9624c3d8787ee734499102ef35ad2d3f36e43a4b
ylcruzdev <ylcruz.dev@gmail.com>
2026-05-07 09:51:43 +0200

    feat: initial structure for incorporating JavaScript

---
+++ src/js/auth.js
@@ -0,0 +1,11 @@
+export const Auth = {
+    
+    users: {
+        email: "ylcruz.dev@gmail.com",
+        password: "12345678",
+    },
+
+    hello(){
+        console.log("hola mundo")
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import { Auth } from "./auth.js"
+
+Auth.hello()
